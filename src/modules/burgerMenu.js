db0f588be8f1336542a79d3b9f411b9764e7b8cc
kirill <muxin.kirill2011@yande.ru>
2020-09-26 19:28:42 +0300

    feature: I added function for closing burger menu and fix adhering burger menu

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -4,9 +4,12 @@ const burgerMenu = () => {
     
     const topMenu = document.querySelector('.top-menu');
     const head = document.querySelector('.head');
+    const popupMenu = document.querySelector('.popup-menu');
 
+
+    window.addEventListener('scroll', function(e) {
     if(window.matchMedia('(max-width: 768px)').matches) {
-        window.addEventListener('scroll', function(e) {
+       
             let cordTopMenu = topMenu.getBoundingClientRect();
             let cordHead = head.getBoundingClientRect();
             if (cordTopMenu.top <= 0) {
@@ -16,7 +19,20 @@ const burgerMenu = () => {
                 topMenu.style.position = 'relative'; 
                 topMenu.style.zIndex = "1";
             }
-        });
-    }
+        }
+    });
+
+    document.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target.closest('.menu-button') || target.closest('.menu-button > img') ) {
+            popupMenu.style.display = 'flex';
+        }
+        if (target.closest('.popup-menu > .close-menu-btn > img') || target.closest('.popup-menu > ul > li') ) {
+            popupMenu.style.display = 'none';
+        }
+        console.log(target);
+    });
+    
 };
 export default burgerMenu;
